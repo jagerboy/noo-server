76c76f0c26729c05855ac9125f540fda5cfd2105
jagerboy <muhammadfachrozy99@gmail.com>
2026-09-15 10:58:09 +0700

    fix: import Schema facade in EdpMasterController

--- app/Http/Controllers/Web/EdpMasterController.php
+++ app/Http/Controllers/Web/EdpMasterController.php
@@ -9,6 +9,7 @@ use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
 use Inertia\Inertia;
 use Inertia\Response;
 use Throwable;
