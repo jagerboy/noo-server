9532980e89900d1b041f07fea2ed7bc44301b613
jagerboy <muhammadfachrozy99@gmail.com>
2026-09-22 14:31:36 +0700

    feat/fix: Perbaikan Layout Signout spv/inbox

--- resources/js/Components/Dropdown.vue
+++ resources/js/Components/Dropdown.vue
@@ -73,7 +73,7 @@ const open = ref(false);
         >
             <div
                 v-show="open"
-                class="absolute z-50 mt-2 rounded-md shadow-lg"
+                class="absolute z-50 mt-2 rounded-md shadow-lg min-w-[170px]"
                 :class="[widthClass, alignmentClasses]"
                 style="display: none"
                 @click="open = false"
